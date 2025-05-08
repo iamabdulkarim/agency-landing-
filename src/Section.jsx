d0187708c76a744b74d777fe5296c0b47a081745
iamabdulkarim <abdulkarim34.fk@gmail.com>
2025-05-08 13:48:16 +0600

    Pricing section and FAQ section completed

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,13 +1,19 @@
 import CompaniesLogoSection from "./CompaniesLogoSection";
+import FaqSection from "./FaqSection";
 import HeroSection from "./HeroSection";
 import "./icon.css";
+import PricingSection from "./PricingSection";
 import ServiceSection from "./ServiceSection";
+import TestimonialSection from "./TestimonialSection";
 export default function Section() {
   return (
     <>
       <HeroSection />
       <CompaniesLogoSection />
       <ServiceSection />
+      <TestimonialSection />
+      <PricingSection />
+      <FaqSection />
     </>
   );
 }
